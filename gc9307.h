e378471386222af9d22f5b7cd311da94882c7a86
Matt Isaacson <misaacson01@gmail.com>
2021-02-06 01:39:18 -0500

    Added build and config options for controlling 2 displays at once (not yet functional)

--- gc9307.h
+++ gc9307.h
@@ -2,16 +2,16 @@
 
 #if defined(GC9307)
 
-// Data specific to the ILI9341 controller (unsure if needed for GC9307 = ???)
+// Data specific to the ILI9341 controller (unsure if needed for GC9307)
 #define DISPLAY_SET_CURSOR_X 0x2A
 #define DISPLAY_SET_CURSOR_Y 0x2B
 #define DISPLAY_WRITE_PIXELS 0x2C
 
-// set width and height (might experience some incorrect offset
+// set width and height (might experience some incorrect offset)
 #define DISPLAY_NATIVE_WIDTH 240
 #define DISPLAY_NATIVE_HEIGHT 210
 
-// ???
+// Not sure what this does
 #define DISPLAY_NATIVE_COVERED_LEFT_SIDE 2
 #define DISPLAY_NATIVE_COVERED_TOP_SIDE 1
 
@@ -22,7 +22,7 @@ void InitGC9307(void);
 void TurnDisplayOn(void);
 void TurnDisplayOff(void);
 
-// ???
+// Not needed
 //#define DISPLAY_NEEDS_CHIP_SELECT_SIGNAL
 
 #endif
